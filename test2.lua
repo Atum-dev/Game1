ed0ddc20092f21d1b5aafec7dd16f003329a0e31
Atum-dev <filipe.alexandre.horta@gmail.com>
2025-01-21 08:17:21 +0000

    Atualizar o test2.lua

--- test2.lua
+++ test2.lua
@@ -1,11 +1,3 @@
-local playerMoney = 100
-
-local fish = 25
-local potato = 70
-
-local items = {fish, potato}
-
-for _, Items in pairs(itens) do
-    local itemPrice = fish + potato
-    print(playerMoney - itemPrice)
-end
+print("Hello, what you want do order?")
+wait(1)
+print("já")
